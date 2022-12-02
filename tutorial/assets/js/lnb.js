25bc230cd953820ea95a07b2b46fd0e965ea1083
DESKTOP-MR73IBF\51 <aluvy_@naver.com>
2022-12-02 14:37:16 +0900

    scrolltrigger 추가

--- tutorial/assets/js/lnb.js
+++ tutorial/assets/js/lnb.js
@@ -1,15 +1,23 @@
 window.addEventListener("load", ()=>{
     const lnbHTML = `
-        <ul>
-            <li class="active"><a href="#gsap-to">gsap.to()</a></li>
-            <li><a href="#gsap-from">gsap.from(), fromTo(), set()</a></li>
-            <li><a href="#gsap-timeline-delay">timeline(delay)</a></li>
-            <li><a href="#gsap-timeline">timeline</a></li>
-        </ul>
-        <ul>
-            <li><a href="#scrolltrigger">scrolltrigger</a></li>
-            <li><a href="#scrolltrigger-toggleactions">toggleActions</a></li>
-        </ul>
+        <section>
+            <h2>Basic</h2>
+            <ul>
+                <li class="active"><a href="#gsap-to">gsap.to()</a></li>
+                <li><a href="#gsap-from">gsap.from(), fromTo(), set()</a></li>
+                <li><a href="#gsap-timeline-delay">timeline(delay)</a></li>
+                <li><a href="#gsap-timeline">timeline</a></li>
+            </ul>
+        </section>
+        <section>
+            <h2>ScrollTrigger</h2>
+            <ul>
+                <li><a href="#scrolltrigger">scrolltrigger</a></li>
+                <li><a href="#scrolltrigger-toggleactions">toggleActions</a></li>
+                <li><a href="#scrolltrigger-scrub">Scrub</a></li>
+                <li><a href="#scrolltrigger-pinning">Pinning</a></li>
+            </ul>
+        </section>
     `;
     let lnb = document.createElement("nav");
     lnb.innerHTML = lnbHTML;
