252ee864d5bec39824a90ddbf7bb5c5422b9841b
songmiok <aluvy_@naver.com>
2024-03-26 16:29:03 +0900

    Update main.js

--- tutorial/motionPath/main.js
+++ tutorial/motionPath/main.js
@@ -6,11 +6,22 @@ const pause = $('#pause');
 const progressBar = $('#progressBar');
 const time = $('#time');
 
-const setButtonText = () => {
+const svg = $('svg');
+
+const site = $$('#home, #mountain, #river, #company');
+
+const path = [
+	{ el: $('#home'), progress: 0 },
+	{ el: $('#mountain'), progress: 0.22 },
+	{ el: $('#river'), progress: 0.47 },
+	{ el: $('#company'), progress: 1 },
+];
+
+const updateButtonText = () => {
 	pause.textContent = animation.paused() || animation.progress() === 1 ? 'play' : 'pause';
 };
 
-const setProgress = () => {
+const updateProgress = () => {
 	time.textContent = animation.time().toFixed(2);
 	progressBar.value = animation.progress();
 };
@@ -21,8 +32,8 @@ const animation = gsap.to(tiger, {
 		path: '#route',
 		align: tiger,
 	},
-	onUpdate: setProgress,
-	onComplete: setButtonText,
+	onUpdate: updateProgress,
+	onComplete: updateButtonText,
 });
 
 pause.addEventListener('click', () => {
@@ -31,15 +42,52 @@ pause.addEventListener('click', () => {
 	} else {
 		animation.paused(!animation.paused());
 	}
-	setButtonText();
+	updateButtonText();
 });
 
 progressBar.addEventListener('input', e => {
-	animation.paused(true);
-	animation.progress(e.target.value);
-	setButtonText();
-	setProgress();
+	animation.progress(e.target.value).paused(true);
+	updateButtonText();
+	updateProgress();
 });
+
+path.forEach((item, i) => {
+	item.el.addEventListener('click', () => {
+		animation.pause();
+		gsap.to(animation, { progress: item.progress, duration: 1 });
+	});
+});
+
+// svg.addEventListener('click', e => {
+// 	const target = e.target.closest('g');
+// 	if (!target) return;
+
+// 	const id = target.getAttribute('id');
+// 	if (!id) return;
+
+// 	let progress = 0;
+
+// 	switch (id) {
+// 		case 'home':
+// 			break;
+// 		case 'mountain':
+// 			progress = 0.22;
+// 			break;
+// 		case 'river':
+// 			progress = 0.47;
+// 			break;
+// 		case 'company':
+// 			progress = 1;
+// 			break;
+// 		default:
+// 			return;
+// 		// break;
+// 	}
+
+// 	animation.pause();
+// 	gsap.to(animation, { progress: progress, duration: 1 });
+// 	updateButtonText();
+// });
 
 // animation.paused(); // getter
 // animation.paused(false); // getter
