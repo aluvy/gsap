16575d84e95a8ddf89e0625cde53b54c8d7fb951
songmiok <aluvy_@naver.com>
2024-04-01 10:33:30 +0900

    Create main.js

---
+++ demo/3D-transform-perspective-text/main.js
@@ -0,0 +1,36 @@
+GSDevTools.create();
+
+gsap.from(".stage", { autoAlpha: 0 });
+
+gsap.registerEffect({
+  name: "text3D",
+  extendTimeline: true,
+  defaults: {
+    deg: 360,
+  },
+  effect: (target, config) => {
+    const split = new SplitText(target, { type: "chars" });
+
+    gsap.set(split.chars, { transformPerspective: 400 });
+
+    const tl = gsap.timeline();
+    tl.to(split.chars, {
+      duration: 1.2,
+      rotationY: config.deg,
+      rotationX: 360,
+      ease: "back(3)",
+      stagger: {
+        amount: 1,
+      },
+    });
+
+    return tl;
+  },
+});
+
+// 1
+// gsap.effects.text3D("h1");
+
+// 2
+const animation = gsap.timeline();
+animation.text3D("h1", { deg: 360 });
