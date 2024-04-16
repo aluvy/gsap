546bbb353bbc899d43a25949fd2dda5ef50dc194
songmiok <aluvy_@naver.com>
2024-04-16 10:11:46 +0900

    Update and rename main.js to main.js

---
+++ demo/util-getunit/main.js
@@ -0,0 +1,8 @@
+const $ = (node) => document.querySelector(node);
+
+let cursor = $("#cursor");
+let intro = $("#intro");
+
+// returns the unit of a CSS value
+// gsap.utils.getUnit("50%"); // "%"
+// gsap.utils.getUnit("100vw"); // "vw"
--- demo/util-method/main.js
+++
@@ -1,4 +0,0 @@
-const $ = (node) => document.querySelector(node);
-
-let cursor = $("#cursor");
-let intro = $("#intro");
