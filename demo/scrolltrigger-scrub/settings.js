7bd21a4a19bfadf57f08e6d3fa68705b47d41fc3
songmiok <aluvy_@naver.com>
2024-04-17 16:13:26 +0900

    Create settings.js

---
+++ demo/scrolltrigger-scrub/settings.js
@@ -0,0 +1,12 @@
+const sectionColors = [
+  "#f2eee5",
+  "#e5c1c5",
+  "#c3e2dd",
+  "#6eceda",
+  "#FB9DA7",
+  "#FCCCD4",
+  "#FBDEA2",
+  "#F2E2C6",
+  "#8EB695",
+];
+gsap.set(".section", { backgroundColor: gsap.utils.wrap(sectionColors) });
