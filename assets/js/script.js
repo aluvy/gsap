e8ad5fd4608c8a6f8a324061c9968f5893189d98
songmiok <aluvy_@naver.com>
2024-04-17 14:14:31 +0900

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,377 @@
 const $ = (node) => document.querySelector(node);
 const $$ = (nodes) => document.querySelectorAll(nodes);
 
-const setVh = () => {
-  document.documentElement.style.setProperty("--vh", `${window.innerHeight}px`);
+document.addEventListener("DOMContentLoaded", () => {
+  setVh();
+  window.addEventListener("resize", setVh);
+
+  setDemo__TIMELINE();
+  setDemo__MOTIONPATH();
+  setDemo__3DTRANSFORM();
+  setDemo__STAGGER();
+  setDemo__KEYFRAMES();
+  setDemo__UTIL();
+  setDemo__ETC();
+
+  handleFloatingIframe();
+});
+
+const setDemo__TIMELINE = () => {
+  setGroup("timeline-area", "Timeline"); // class, title
+
+  const area = $(".demogroup.timeline-area ul");
+  const item = [
+    {
+      title: "TOYSTORY",
+      href: "./demo/timeline-toystory/",
+      hash: ["timeline"],
+    },
+    {
+      title: "함수 중심의 타임라인 설계",
+      href: "./demo/timeline-master2/",
+      hash: ["master timeline2", "TextPlugin", "배열 구조 분해 할당"],
+    },
+    {
+      title: "타임라인 효율적으로 관리하기",
+      href: "./demo/timeline-master/",
+      hash: ["master timeline", "timeline.add()"],
+    },
+    {
+      title: "timeline addlabel",
+      href: "./demo/timeline-addlabel/",
+      hash: ["add()", "addPause()", "timeline.play()", "timeline.reverse()"],
+    },
+    {
+      title: "Van Cleef & Arpels",
+      href: "./demo/timeline-van-cleef-arpels/",
+      hash: ["autoAlpha"],
+    },
+    {
+      title: "planet",
+      href: "./demo/timeline-planet/",
+      hash: ["autoAlpha", "timeline({defaults})"],
+    },
+    {
+      title: "timeline Visualizer",
+      href: "./demo/timeline-visualizer/",
+      hash: [
+        "timeline callback",
+        "eventCallback",
+        "draggable",
+        "drag",
+        "timeline.time()",
+      ],
+    },
+    {
+      title: "slide animation",
+      href: "./demo/timeline-slide/",
+      hash: [
+        "GSDevTools",
+        "gsap.registerEffect",
+        "SplitText",
+        "timeline.eventCallback",
+      ],
+    },
+  ];
+  setItem(area, item);
 };
 
-setVh();
-window.addEventListener("resize", setVh);
+const setDemo__MOTIONPATH = () => {
+  setGroup("motionpath-area", "MotionPath"); // class, title
 
-function handleFloatingIframe() {
+  const area = $(".demogroup.motionpath-area ul");
+  const item = [
+    {
+      title: "Tiger",
+      href: "./demo/motionpath-tiger/",
+      hash: [
+        "svg",
+        "motionPath",
+        "onUpdate",
+        "onComplete",
+        "tween callback",
+        "timeline.paused()",
+        "timeline.progress()",
+        "timeline.restart()",
+        "gsap.to(timeline, {})",
+      ],
+    },
+    {
+      title: "Map",
+      href: "./demo/motionpath-map/",
+      hash: [
+        "timeline",
+        "timeline.pause()",
+        "strokeDashoffset",
+        "path",
+        "align",
+        "alignOrigin",
+        "svg.getTotalLength()",
+      ],
+    },
+  ];
+  setItem(area, item);
+};
+
+const setDemo__3DTRANSFORM = () => {
+  setGroup("transform-area", "3D Transform"); // class, title
+
+  const area = $(".demogroup.transform-area ul");
+  const item = [
+    {
+      title: "3D transform - box",
+      href: "./demo/3D-transform-perspective-box/",
+      hash: ["3D", "transformPerspective", "transformOrigin", "rotationY"],
+    },
+    {
+      title: "3D transform - text",
+      href: "./demo/3D-transform-perspective-text/",
+      hash: [
+        "autoAlpha",
+        "registerEffect",
+        "extendTimeline",
+        "timeline",
+        "SplitText",
+        "transformPerspective",
+        "stagger",
+        "amount",
+        "rotation",
+      ],
+    },
+    {
+      title: "3D text",
+      href: "./demo/3D-text/",
+      hash: [
+        "stagger 심화",
+        "타임라인 재생시간 컨트롤",
+        "< 0.5",
+        "rotationX",
+        "rotationY",
+        "transformOrigin",
+        "stagger.repeatDelay",
+        "stagger repeatDelay = stagger * (numberOfTargets - 1) + pause;",
+      ],
+    },
+  ];
+  setItem(area, item);
+};
+
+const setDemo__STAGGER = () => {
+  setGroup("stagger-area", "Stagger"); // class, title
+
+  const area = $(".demogroup.stagger-area ul");
+  const item = [
+    {
+      title: "stagger space",
+      href: "./demo/stagger-space/",
+      hash: ["insertAdjacentHTML"],
+    },
+    {
+      title: "stagger ease",
+      href: "./demo/stagger-ease/",
+      hash: ["each", "amount"],
+    },
+    {
+      title: "./demo/stagger-ease-interstellar/",
+      href: "stagger interstellar",
+      hash: [
+        "timeline",
+        "gsap.utils.distribute()",
+        "base",
+        "amount",
+        "SplitText",
+        "gsap.utils.wrap([])",
+        "delay",
+      ],
+    },
+    {
+      title: "stagger repeat",
+      href: "./demo/stagger-repeat/",
+      hash: ["each", "repeat", "yoyo", "grid"],
+    },
+    {
+      title: "stagger callback",
+      href: "./demo/stagger-callback/",
+      hash: [
+        "tween callback",
+        "stagger callback",
+        "onComplete",
+        "onStart",
+        "SplitText",
+        "stagger.from:random",
+        "gsap attr plugin",
+        "startTime",
+      ],
+    },
+  ];
+  setItem(area, item);
+};
+
+const setDemo__KEYFRAMES = () => {
+  setGroup("keyframes-area", "KeyFrames"); // class, title
+
+  const area = $(".demogroup.keyframes-area ul");
+  const item = [
+    {
+      title: "keyframes1",
+      href: "./demo/keyframes1/",
+      hash: [
+        "among us",
+        "timeline",
+        "autoAlpha",
+        "keyframes",
+        "stagger",
+        "amount",
+        "each",
+      ],
+    },
+    {
+      title: "keyframes2",
+      href: "./demo/keyframes2/",
+      hash: [
+        "among us",
+        "timeline",
+        "autoAlpha",
+        "keyframes",
+        "easeEach",
+        "amount",
+      ],
+    },
+    {
+      title: "keyframes3",
+      href: "./demo/keyframes3/",
+      hash: ["among us", "timeline", "autoAlpha", "keyframes", "amount"],
+    },
+  ];
+  setItem(area, item);
+};
+
+const setDemo__UTIL = () => {
+  setGroup("util-area", "Util"); // class, title
+
+  const area = $(".demogroup.util-area ul");
+  const item = [
+    {
+      title: "gsap.utils.mapRange()",
+      href: "./demo/util-maprange/",
+      hash: [
+        "gsap.utils.mapRange()",
+        "중복된 트윈을 지속적으로 만들어야 할 때는 gsap.quickTo() 사용하는게 좋다",
+        "x-ray 효과",
+      ],
+    },
+    {
+      title: "gsap.utils.getUnit()",
+      href: "./demo/util-getunit/",
+      hash: ["gsap.utils.getUnit()", "단위가 반환된다"],
+    },
+    {
+      title: "gsap.utils.wrap()",
+      href: "./demo/util-wrap/",
+      hash: ["gsap.utils.wrap([])", "stagger", "each", "timeline", "SplitText"],
+    },
+    {
+      title: "gsap.utils.distribute()",
+      href: "./demo/util-distribute/",
+      hash: [
+        "gsap.utils.distribute({}): 값을 알아서 분배해준다.",
+        "base",
+        "amount",
+        "ease",
+        "stagger",
+        "each",
+      ],
+    },
+    {
+      title: "gsap.registerEffect()",
+      href: "./demo/util-register-effect/",
+      hash: [
+        "gsap.registerPlugin()",
+        "gsap.registerEffect({})",
+        "extendTimeline",
+        "SplitText",
+        "stagger",
+        "each",
+        "gsap.utils.wrap()",
+      ],
+    },
+    {
+      title: "css plugin: clearProps",
+      href: "./demo/util-css-plugin/",
+      hash: ["gsap.utils.toArray()", "clearProps"],
+    },
+  ];
+  setItem(area, item);
+};
+
+const setDemo__ETC = () => {
+  setGroup("etc-area", "ETC"); // class, title
+
+  const area = $(".demogroup.etc-area ul");
+  const item = [
+    {
+      title: "gsap.delayedCall()",
+      href: "./demo/etc-delayedcall/",
+      hash: [
+        "몇 초 뒤에 다른 함수를 호출해야 될 경우",
+        "setTimeout과 유사하지만 다름",
+        "addPause()",
+      ],
+    },
+    {
+      title: "textplugin",
+      href: "./demo/etc-textplugin/",
+      hash: ["delayedCall()", "array.push(array.shift())"],
+    },
+    {
+      title: "callback",
+      href: "./demo/etc-callback/",
+      hash: ["onComplete", "onCompleteParams", "static method", "class"],
+    },
+    {
+      title: "runtime rendering - immediateRender",
+      href: "./demo/etc-immediateRender/",
+      hash: ["timeline", "immediateRender: false"],
+    },
+    {
+      title: "function based value",
+      href: "./demo/etc-function-based-value/",
+      hash: ["함수 기반의 값", "stagger"],
+    },
+    // {
+    //   title: "title",
+    //   href: "href",
+    //   hash: ["hash"],
+    // },
+  ];
+  setItem(area, item);
+};
+
+const setGroup = (area, ttl) => {
+  // class, title
+  const demoArea = $(".demo-area");
+  const HTML = `<div class="demogroup ${area}"><h3>${ttl}</h3><ul></ul></div>`;
+  demoArea.insertAdjacentHTML("beforeend", HTML);
+};
+
+const setItem = (area, item) => {
+  // dom, Object
+  item.forEach((a) => {
+    const HTML = `
+      <li>
+        <a href="${a.href}" target="_blank" title="새 창 열림">
+          <strong>${a.title}</strong>
+          <p class="hash">${a.hash
+            .map((hash) => `<span>${hash}</span>`)
+            .join("")}</p>
+        </a>
+      </li>
+    `;
+    area.insertAdjacentHTML("beforeend", HTML);
+  });
+};
+
+const handleFloatingIframe = () => {
   const float = $("#float");
   const iframe = $("#float iframe");
   const a = Array.from($$(".demogroup a"));
@@ -16,6 +379,7 @@ function handleFloatingIframe() {
   a.forEach((elem) => {
     elem.addEventListener("mouseenter", (e) => {
       const url = elem.getAttribute("href");
+      if (url === "href") return;
       iframe.setAttribute("src", url);
       float.style.display = "block";
     });
@@ -33,5 +397,8 @@ function handleFloatingIframe() {
       float.style.display = "none";
     });
   });
-}
-handleFloatingIframe();
+};
+
+const setVh = () => {
+  document.documentElement.style.setProperty("--vh", `${window.innerHeight}px`);
+};
