3bd4c291a7910101b9c7e35c8cbe1512f4203553
songmiok <aluvy_@naver.com>
2024-04-24 09:45:36 +0900

    Update main.js

--- demo/scrolltrigger-callback/main.js
+++ demo/scrolltrigger-callback/main.js
@@ -4,7 +4,11 @@ gsap.to(h2, {
   x: 200,
   scrollTrigger: {
     trigger: ".section02",
-    start: "20% center",
+    // start: "20% center",
+    start: () => {
+      console.log("start"); // scrollTrigger는 resize 할 때 마다 start 값을 갱신한다.
+      return "20% center";
+    },
     end: "80% center",
     scrub: true,
     markers: true,
@@ -12,14 +16,61 @@ gsap.to(h2, {
     /**
      * onEnter: 시작점을 지날 때
      */
-    onEnter() {
-      h2.textContent = "enter!";
+    onEnter(self) {
+      h2.textContent = "Enter!";
+      console.log(self);
     },
     /**
      * onLeave: 종료점을 지날 때
      */
-    onLeave() {
-      console.log("leave!");
+    onLeave(self) {
+      h2.textContent = "Leave!";
+      console.log(self);
+    },
+    /**
+     * onEnterBack: 다시 들어올 때
+     */
+    onEnterBack(self) {
+      h2.textContent = "EnterBack!";
+      console.log(self);
+    },
+    /**
+     * onLeaveBack: 다시 나갈 때
+     */
+    onLeaveBack(self) {
+      h2.textContent = "LeaveBack!";
+      console.log(self);
+    },
+    /**
+     * onToggle: onEnter, onLeave, onEnterBack, onLeaveBack 콜백들이 실행될 때 마다 실행됨
+     * direction: 스크롤 방향 체크 (내리면 양수, 올리면 음수)
+     */
+    onToggle({ direction }) {
+      // console.log("toggle", direction);
+      if (direction === 1) {
+        h2.style.color = "red";
+      } else {
+        h2.style.color = "blue";
+      }
+    },
+    /**
+     * onRefresh: resize 됐을 때
+     */
+    onRefresh(self) {
+      console.log("onRefresh");
+    },
+    /**
+     * onUpdate: 스크롤이 되는 동안 지속적으로 값을 업데이트 시키고 찍어줌
+     * progress: 0 ~ 1
+     */
+    onUpdate({ progress }) {
+      let percentage = Math.round(progress * 100);
+      h2.textContent = `${percentage}%`;
+      if (percentage > 50) {
+        gsap.set(".section02", { backgroundColor: "orange" });
+      } else {
+        gsap.set(".section02", { backgroundColor: "hotpink" });
+      }
     },
   },
 });
