91b9243c68660f2dd92e8253bca32ad9b6ddf1e3
songmiok <aluvy_@naver.com>
2024-05-09 15:50:08 +0900

    Update main.js

--- demo/scrolltrigger-NavigationLoop/main.js
+++ demo/scrolltrigger-NavigationLoop/main.js
@@ -1,6 +1,7 @@
 gsap.registerPlugin(ScrollTrigger);
 
 const navColor = ['#ebdec1', '#e9aab1', '#92e0d3', '#52becb', '#f17683'];
+const section = gsap.utils.toArray('.section').map(o => o.getBoundingClientRect().top);
 
 const nav = document.querySelector('.nav');
 const navH = nav.clientHeight;
@@ -16,6 +17,15 @@ gsap.utils.toArray('.section').forEach((o, i) => {
 	});
 });
 
+gsap.utils.toArray('.nav li a').forEach((o, i) => {
+	o.addEventListener('click', () => {
+		// const y = document.querySelectorAll('.section')[i].offsetTop;
+
+		const y = ScrollTrigger.getAll()[i].start + nav.scrollHeight;
+		scrollbar.scrollTo(0, y, 600);
+	});
+});
+
 markers();
 
 /**
@@ -23,4 +33,8 @@ markers();
  *
  * 스크롤 트리거는 트리거가 생성되는 시점(처음 코드가 읽히는 시점)에 from의 위치를 미리 계산한다.
  * immediateRender: false 로 설정하여, from의 위치를 제어할 수 있다.
+ *
+ * 2. 클릭 이벤트
+ * 스크롤 트리거는 스크롤에 관련된 값들을 전부 계산해놓고 있고, 브라우저 resize 이벤트도 적용되어 있기 때문에 section의 start 값을 가져와서 사용할 수 있다.
+ * const y = ScrollTrigger.getAll()[i].start + nav.scrollHeight;
  */
