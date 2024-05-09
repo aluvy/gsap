7715f6fe015f5b5c4d9286df9eeb11db1e489c6c
songmiok <aluvy_@naver.com>
2024-05-09 13:54:02 +0900

    Create settings.js

---
+++ demo/scrollsmoother-test/settings.js
@@ -0,0 +1,17 @@
+const settings = () => {
+	const sectionColors = ['#f2eee5', '#e5c1c5', '#c3e2dd', '#6eceda', '#FB9DA7', '#FCCCD4', '#FBDEA2', '#F2E2C6', '#8EB695'];
+
+	gsap.set('.section', { backgroundColor: gsap.utils.wrap(sectionColors) });
+
+	gsap.utils.toArray('.section').forEach((item, index) => {
+		let h2 = /* html */ `<h2>section${index + 1}</h2>`;
+		item.insertAdjacentHTML('beforeend', h2);
+	});
+};
+settings();
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+ScrollSmoother.create({
+	smooth: 1,
+	effects: true,
+});
