b62cc16f26774b1d387a611760588247a1308817
songmiok <aluvy_@naver.com>
2024-05-28 09:58:28 +0900

    Update main.js

--- demo/layout-horizontal-plugin/main.js
+++ demo/layout-horizontal-plugin/main.js
@@ -1,12 +1,14 @@
+ScrollTrigger.create({
+  trigger: '.section02',
+  start: 'left center',
+  end: 'right center',
 
+  // horizontal scroll
+  horizontal: true,
 
+  animation: gsap.to('.box', { rotation: 360 }),
+  scrub: true,
+  markers: true,
+})
 
-
-
-
-
-
-
-
-markers()
-
+markers();
