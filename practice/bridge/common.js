9fee1a44962488809df91d11915574f08aca47c9
songmiok <aluvy_@naver.com>
2024-05-29 17:04:42 +0900

    Update common.js

--- practice/bridge/common.js
+++ practice/bridge/common.js
@@ -1,1 +1,39 @@
 
+
+const front = {
+  init() {
+    gsap.set(container, { autoAlpha: 1 })
+    window.addEventListener('resize', front.debounce(front.setVh, 100));
+    front.setVh();
+  },
+
+  setVh() {
+    document.documentElement.style.setProperty('--vh', `${window.innerHeight}px`);
+  },
+
+  debounce(callback, time = 500) {
+    let timeout
+    return function(...args) {
+      clearTimeout(timeout);
+      timeout = setTimeout(()=>{
+        callback.apply(this, args);
+      }, time)
+    }
+  },
+  
+  throttle(callback, limit = 100) {
+    let waiting = false;
+
+    return function() {
+      if(!waiting) {
+        callback.apply(this, arguments)
+        waiting = true
+        setTimeout(()=>{
+          waiting = false
+        }, limit)
+      }
+    }
+  },
+}
+
+document.addEventListener("DOMContentLoaded", front.init);
