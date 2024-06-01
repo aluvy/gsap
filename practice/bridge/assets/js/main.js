b2b05f520f28f44a2a332d98dc1b15c07efc02b4
miok <aluvy_@naver.com>
2024-06-01 18:36:57 +0900

    wall update

--- practice/bridge/assets/js/main.js
+++ practice/bridge/assets/js/main.js
@@ -8,98 +8,98 @@ const breakPoint = {
 const intro = {
   init() {
     intro.animation();
+    window.addEventListener("resize", front.debounce(gsap.matchMediaRefresh, 100));
   },
   animation() {
     const bridge = document.querySelector("#intro h1");
     if ( is.none(bridge) ) return;
 
-    const chars = front.spitText(bridge);
+    front.spitText(bridge);
     const mm = gsap.matchMedia();
+
+    const trigger = document.querySelector("#intro");
 
     mm.add(breakPoint, (ctx) => {
       const { isDesktop, isTablet, isMobile, reduceMotion } = ctx.conditions;
 
       if(reduceMotion) return;
-      if ( isDesktop )        ST_desktop();
-      else if ( isTablet )    ST_tablet();
-      else if ( isMobile )    ST_mobile();
-
+      if ( isDesktop )        intro.$desktop(trigger);
+      else if ( isTablet )    intro.$tablet(trigger);
+      else if ( isMobile )    intro.$mobile(trigger);
     });
-
-    // desktop
-    function ST_desktop() {
-      const tl = gsap.timeline({ defaults: { scale: 13, transformOrigin: "50% 50%" } });
-      tl.from(".char-b", { xPercent: -250, yPercent: 0 })
-        .from(".char-r", { xPercent: 0, yPercent: -1000 }, 0.1)
-        .from(".char-i", { xPercent: 1000, yPercent: -1000 }, 0.15)
-        .from(".char-d", { xPercent: -1000, yPercent: -1000 }, 0.2)
-        .from(".char-g", { xPercent: -1000, yPercent: 1500 }, 0.25)
-        .from(".char-e", { xPercent: 1000, yPercent: 500 }, 0.3)
-        .from("#intro .desc", { autoAlpha: 0, y: 30, scale: 1, duration: 0.1 })
-
-      ScrollTrigger.create({
-        trigger: '#intro',
-        start: 'top top',
-        end: `+=3000`,
-        animation: tl,
-        pin: !0,
-        scrub: 1,
-        // markers: true,
-      })
-      markers();
-    }
-
-    // tablet
-    function ST_tablet() {
-      const tl = gsap.timeline({ defaults: { scale: 16, transformOrigin: "50% 50%" } })
-        .from(".char-b", { xPercent: -150, yPercent: 0 })
-        .from(".char-r", { xPercent: 0, yPercent: -2000 }, 0.1)
-        .from(".char-i", { xPercent: 2000, yPercent: -1000 }, 0.15)
-        .from(".char-d", { xPercent: -2000, yPercent: -1000 }, 0.2)
-        .from(".char-g", { xPercent: -2000, yPercent: 1500 }, 0.25)
-        .from(".char-e", { xPercent: 2000, yPercent: 500 }, 0.3)
-        .from("#intro .desc", { autoAlpha: 0, y: 30, scale: 1, duration: 0.1 })
-
-      ScrollTrigger.create({
-        trigger: '#intro',
-        start: 'top top',
-        end: `+=3000`,
-        animation: tl,
-        pin: !0,
-        scrub: 1,
-        // markers: true,
-      })
-      markers();
-    }
-
-    // mobile
-    function ST_mobile() {
-      const tl = gsap.timeline({ defaults: { scale: 20, transformOrigin: "50% 50%" } })
-        .from(".char-b", { xPercent: -150, yPercent: 0 })
-        .from(".char-r", { xPercent: 0, yPercent: -3000 }, 0.1)
-        .from(".char-i", { xPercent: 3000, yPercent: -2000 }, 0.15)
-        .from(".char-d", { xPercent: -3000, yPercent: -1000 }, 0.2)
-        .from(".char-g", { xPercent: -3000, yPercent: 1500 }, 0.25)
-        .from(".char-e", { xPercent: 3000, yPercent: 500 }, 0.3)
-        .from("#intro .desc", { autoAlpha: 0, y: 30, scale: 1, duration: 0.1 })
-
-      ScrollTrigger.create({
-        trigger: '#intro',
-        start: 'top top',
-        end: `+=3000`,
-        animation: tl,
-        pin: !0,
-        scrub: 1,
-        // markers: true,
-      })
-      markers();
-    }
   },
+  $desktop(trigger) {
+    const tl = gsap.timeline({ defaults: { scale: 13, transformOrigin: "50% 50%" } });
+    tl.from(".char-b", { xPercent: -250, yPercent: 0 })
+      .from(".char-r", { xPercent: 0, yPercent: -1000 }, 0.1)
+      .from(".char-i", { xPercent: 1000, yPercent: -1000 }, 0.15)
+      .from(".char-d", { xPercent: -1000, yPercent: -1000 }, 0.2)
+      .from(".char-g", { xPercent: -1000, yPercent: 1500 }, 0.25)
+      .from(".char-e", { xPercent: 1000, yPercent: 500 }, 0.3)
+      .from("#intro .desc", { autoAlpha: 0, y: 30, scale: 1, duration: 0.1 })
+
+    ScrollTrigger.create({
+      trigger: trigger,
+      start: 'top top',
+      end: `+=3000`,
+      animation: tl,
+      pin: !0,
+      scrub: 1,
+      // markers: true,
+    })
+    // markers();
+  },
+  $tablet(trigger) {
+    const tl = gsap.timeline({ defaults: { scale: 16, transformOrigin: "50% 50%" } })
+      .from(".char-b", { xPercent: -150, yPercent: 0 })
+      .from(".char-r", { xPercent: 0, yPercent: -2000 }, 0.1)
+      .from(".char-i", { xPercent: 2000, yPercent: -1000 }, 0.15)
+      .from(".char-d", { xPercent: -2000, yPercent: -1000 }, 0.2)
+      .from(".char-g", { xPercent: -2000, yPercent: 1500 }, 0.25)
+      .from(".char-e", { xPercent: 2000, yPercent: 500 }, 0.3)
+      .from("#intro .desc", { autoAlpha: 0, y: 30, scale: 1, duration: 0.1 })
+
+    ScrollTrigger.create({
+      trigger: trigger,
+      start: 'top top',
+      end: `+=3000`,
+      animation: tl,
+      pin: !0,
+      scrub: 1,
+      // markers: true,
+    })
+    // markers();
+  },
+  $mobile(trigger) {
+    const tl = gsap.timeline({ defaults: { scale: 20, transformOrigin: "50% 50%" } })
+      .from(".char-b", { xPercent: -150, yPercent: 0 })
+      .from(".char-r", { xPercent: 0, yPercent: -3000 }, 0.1)
+      .from(".char-i", { xPercent: 3000, yPercent: -2000 }, 0.15)
+      .from(".char-d", { xPercent: -3000, yPercent: -1000 }, 0.2)
+      .from(".char-g", { xPercent: -3000, yPercent: 1500 }, 0.25)
+      .from(".char-e", { xPercent: 3000, yPercent: 500 }, 0.3)
+      .from("#intro .desc", { autoAlpha: 0, y: 30, scale: 1, duration: 0.1 })
+
+    ScrollTrigger.create({
+      trigger: trigger,
+      start: 'top top',
+      end: `+=3000`,
+      animation: tl,
+      pin: !0,
+      scrub: 1,
+      // markers: true,
+    })
+    // markers();
+  },
+  $reduceMotion(trigger) {
+
+  }
 };
 
 const wall = {
   init() {
     wall.animation();
+    window.addEventListener("resize", front.debounce(gsap.matchMediaRefresh, 100));
   },
   animation() {
     const wallItem = document.querySelector("#wall .wall-item");
@@ -108,29 +108,186 @@ const wall = {
     const items = document.querySelectorAll("#wall .wall-item:not(.show)");
     items.forEach( a => front.spitText(a) );
 
-    gsap.set("#wall .wall-side .chars", { opacity: 0 });
+    const _el = {
+      wall: document.querySelector("#wall"),
+      wallWrapper: document.querySelector("#wall .wall-wrapper"),
+      wallFront: document.querySelector("#wall .wall-front"),
+      wallFrontChars: document.querySelectorAll("#wall .wall-front .chars"),
+      wallSide: document.querySelector("#wall .wall-side"),
+      wallSideChars: document.querySelectorAll("#wall .wall-side .chars"),
+      number: document.querySelector("#wall .wall-front .number"),
+    }
 
-    const tl = gsap.timeline();
-    tl.from("#wall .wall-front .wall-item .chars", {
-      opacity: 0,
-      duration: 1,
-      stagger: {
-        each: 0.1,
-        from: "random",
-        easel: "power1",
-      }
+    const mm = gsap.matchMedia();
+
+    mm.add(breakPoint, (ctx) => {
+      const { isDesktop, isTablet, isMobile, reduceMotion } = ctx.conditions;
+
+      if (reduceMotion)         wall.$reduceMotion(_el);
+      else if ( isDesktop )     wall.$desktop(_el);
+      else if ( isTablet )      wall.$tablet(_el);
+      else if ( isMobile )      wall.$mobile(_el);
     });
 
+  },
+  $desktop(_el) {
+    const { wall, wallWrapper, wallFront, wallFrontChars, wallSide, wallSideChars, number } = _el;
+
+    const mousePosition = { x: 0, y: 0 };
+    function handleMouseMove(e) {
+      mousePosition.x = -1 + e.clientX / document.documentElement.clientWidth * 2,
+      mousePosition.y = 1 - e.clientY / document.documentElement.clientHeight * 2,
+      gsap.to(wallWrapper, { rotationZ: mousePosition.x * 5, rotationX: mousePosition.y * 5 }),
+      gsap.to(wallWrapper, { rotationZ: 0, rotationX: 0 })
+    }
+
+    gsap.set(wallSide, { x: wallFront.offsetWidth / 2, rotationY: "90deg" })
+    gsap.set(wallFront, { z: wallSide.offsetWidth / 2 })
+
+    const numberCount = gsap.from(number, {
+      textContent: 177000,
+      duration: 2,
+      snap: { textContent: 1 },
+      scrollTrigger: {
+        trigger: wall,
+        start: "top bottom",
+        toggleActions: "play none none reset",
+      }
+    })
+
+    const tl = gsap.timeline()
+      .from(wallFrontChars, {
+        opacity: 0,
+        stagger: { each: 0.1, from: "random" },
+        onUpdate: () => {
+          numberCount.progress(1),
+          wall.removeEventListener("mousemove", handleMouseMove),
+          gsap.to(wallWrapper, { rotationZ: 0, rotationX: 0 })
+        },
+        onComplete: () => wall.addEventListener("mousemove", handleMouseMove),
+      })
+      .to(wallWrapper, { rotationY: -90, duration: 15 })
+      .to(wallSideChars, {
+        opacity: 0,
+        stagger: { each: 0.1, from: "random" },
+        onStart: () => {
+          wall.removeEventListener("mousemove", handleMouseMove),
+          gsap.to(wallWrapper, { rotationZ: 0, rotationX: 0 })
+        },
+        onReverseComplete: () => wall.addEventListener("mousemove", handleMouseMove),
+      });
+
     ScrollTrigger.create({
-      trigger: '#wall',
+      trigger: wall,
       start: 'top top',
       end: `+=2000`,
       animation: tl,
-      pin: true,
-      scrub: true,
-      markers: true
+      pin: !0,
+      scrub: !0,
+      // markers: true
     })
-    markers();
+    // markers();
+
+  },
+  $tablet(_el) {
+    const { wall, wallWrapper, wallFront, wallFrontChars, wallSide, wallSideChars, number } = _el;
+
+    gsap.set(wallSide, { x: wallFront.offsetWidth / 2, rotationY: "90deg" })
+    gsap.set(wallFront, { z: wallSide.offsetWidth / 2 })
+    
+    // number count
+    gsap.from(number, {
+      textContent: 177000,
+      duration: .8,
+      snap: { textContent: 1 },
+      scrollTrigger: {
+        trigger: wall,
+        start: "top bottom",
+        toggleActions: "play none none reset"
+      }
+    })
+
+    const tl = gsap.timeline()
+      .from(wallFrontChars, {
+        opacity: 0,
+        stagger: { each: .1, from: "random" },
+        onUpdate: () => gsap.to(wallWrapper, { rotationZ: 0, rotationX: 0 }),
+      })
+      .to(wallWrapper, { rotationY: -90, duration: 15 })
+      .to(wallSideChars, {
+        opacity: 0,
+        stagger: { each: .1, from: "random" },
+        onStart: () => gsap.to(wallWrapper, { rotationZ: 0, rotationX: 0 }),
+      });
+    
+    ScrollTrigger.create({
+      trigger: wall,
+      start: "top top",
+      end: "+=2000",
+      animation: tl,
+      pin: !0,
+      pinSpacing: !0,
+      scrub: !0,
+    })
+  },
+  $mobile(_el) {
+    const { wall, wallWrapper, wallFront, wallFrontChars, wallSide, wallSideChars, number } = _el;
+
+    gsap.set(wallSide, { x: wallFront.offsetWidth / 2, rotationY: "90deg" })
+    gsap.set(wallFront, { z: wallSide.offsetWidth / 2 })
+
+    // numberCount
+    gsap.from(number, {
+      textContent: 177000,
+      duration: .8,
+      snap: { textContent: 1 },
+      scrollTrigger: {
+        trigger: wall,
+        start: "top bottom",
+        toggleActions: "play none none reset"
+      }
+    })
+
+    const tl = gsap.timeline()
+      .from(wallFrontChars, {
+        opacity: 0,
+        stagger: { each: .1, from: "random" },
+        onUpdate: () => gsap.to(".wall-wrapper", { rotationZ: 0, rotationX: 0 }),
+      })
+      .to(wallWrapper, { rotationY: -90, duration: 15 })
+      .to(wallSideChars, {
+        opacity: 0,
+        stagger: { each: .1, from: "random" },
+        onStart: () => gsap.to(wallWrapper, { rotationZ: 0, rotationX: 0 }),
+      });
+
+    ScrollTrigger.create({
+      trigger: wall,
+      start: "top top",
+      end: "+=2000",
+      animation: tl,
+      pin: !0,
+      pinSpacing: !0,
+      scrub: !0,
+      // markers: true
+    })
+    // markers();
+  },
+  $reduceMotion (_el) {
+    const { wall, wallWrapper, wallFront, wallFrontChars, wallSide, wallSideChars, number } = _el;
+
+    let tl = gsap.timeline()
+      .to(wallFront, { autoAlpha: 0, delay: 1 })
+      .from(wallSide, { autoAlpha: 0, duration: 2 }, "-=0.3");
+
+    ScrollTrigger.create({
+      trigger: wall,
+      start: "top top",
+      end: "+=2000",
+      animation: tl,
+      pin: !0,
+      scrub: !0
+    }) 
   }
 }
 
@@ -194,8 +351,6 @@ const qode = {
   },
   animation() {}
 }
-
-
 
 
 function init() {
